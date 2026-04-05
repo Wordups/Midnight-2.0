0ed23cfd93586d7045be355d4749b48fb0663d47
Brian Word <148787076+Wordups@users.noreply.github.com>
2026-04-05 19:41:39 -0400

    Update supabase_client.py

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -1,6 +1,6 @@
 from supabase import create_client
 
-SUPABASE_URL = "YOUR_URL"
-SUPABASE_KEY = "YOUR_ANON_KEY"
+SUPABASE_URL = "https://YOUR_PROJECT.supabase.co"
+SUPABASE_KEY = "YOUR_KEY"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
