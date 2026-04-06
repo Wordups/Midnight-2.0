31affbd8fe744cc2b9956e87a9076a3a262587a4
Brian Word <148787076+Wordups@users.noreply.github.com>
2026-04-05 23:28:14 -0400

    Update supabase_client.py

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -2,7 +2,7 @@ import os
 from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL", "https://devphpxqlpcvsuuzkwyo.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcHZocHhxbHBjdnN1dXprd3lvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0Mjk1NDksImV4cCI6MjA5MTAwNTU0OX0.2OzdwVVjbGpdT6LyCioAj8bfqL8ZLWHXWkKeWLRGuWQ")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcHZocHhxbHBjdnN1dXprd3lvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTQyOTU0OSwiZXhwIjoyMDkxMDA1NTQ5fQ.HDoNM9zDYYVK5BnDW-uZAgLVPIDcq857ijN2-kGzd78")
 
 supabase = None
 if SUPABASE_URL and SUPABASE_KEY:
