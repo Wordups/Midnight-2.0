042205deaedb9d5fb5db711b537e1957fd08e53f
Brian Word <148787076+Wordups@users.noreply.github.com>
2026-04-05 23:33:47 -0400

    Update supabase_client.py

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -2,7 +2,7 @@ import os
 from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL", "https://devphpxqlpcvsuuzkwyo.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcHZocHhxbHBjdnN1dXprd3lvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTQyOTU0OSwiZXhwIjoyMDkxMDA1NTQ5fQ.HDoNM9zDYYVK5BnDW-uZAgLVPIDcq857ijN2-kGzd78")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
 
 supabase = None
 if SUPABASE_URL and SUPABASE_KEY:
